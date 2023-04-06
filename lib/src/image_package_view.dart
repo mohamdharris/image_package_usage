3fc3822fccdac64f134be7f6a5a1971afa121fa2
harris_trirope <harris@trirope.com>
2023-04-06 15:40:14 +0530

    init

---
+++ lib/src/image_package_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ImagePackage extends StatefulWidget {
+  const ImagePackage({super.key});
+
+  @override
+  State<ImagePackage> createState() => _ImagePackageState();
+}
+
+class _ImagePackageState extends State<ImagePackage> {
+    Future<void> loadAsset() async {
+    final assetData = await rootBundle.load('assets/logo.jpg');
+
+    // Do something with the asset data
+    print("asserts loading from load Asset");
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height*0.50,
+      width: MediaQuery.of(context).size.width*0.50,
+      color: Colors.amber,
+      child: const Image(image: AssetImage('assets/logo.jpg')),
+    );
+  }
+}
